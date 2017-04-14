ff12f99bf2b63b321526f127cf628e7c10141035
Edisson Reinozo <edisson350@gmail.com>
2017-04-14 18:38:09 -0500

    'add app.py'

--- app.py
+++ app.py
@@ -1,68 +1,111 @@
-# local wit.ai app. to develop and test funtionality
-
+import os
 from wit import Wit
-import fb
-import random
-WIT_TOKEN = 'JBO7M7LFWCMLK53MQBYV7MECVA4JSDIU'
+import requests
+from bottle import Bottle, request, debug
+
+
+# Declare some constants
+FB_VERIFY_TOKEN = os.environ['FB_VERIFY_TOKEN']
+FB_PAGE_ID = os.environ['FB_PAGE_ID']
+FB_ACCESS_TOKEN = os.environ['FB_ACCESS_TOKEN']
+WIT_TOKEN = os.environ['WIT_TOKEN']
+
+# Setup Bottle Server
+debug(True)
+app = Bottle()
+
+
+# Facebook Messenger GET Webhook
+@app.get('/webhook')
+def messenger_webhook():
+    """
+    A webhook to return a challenge
+    """
+    verify_token = request.query.get('hub.verify_token')
+    # check whether the verify tokens match
+    if verify_token == FB_VERIFY_TOKEN:
+        # respond with the challenge to confirm
+        challenge = request.query.get('hub.challenge')
+        return challenge
+    else:
+        return 'Invalid Request or Verification Token'
+
+
+# Facebook Messenger POST Webhook
+@app.post('/webhook')
+def messenger_post():
+    """
+    Handler for webhook (currently for postback and messages)
+    """
+    data = request.json
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            # get all the messages
+            messages = entry['messaging']
+            if messages[0]:
+                # Get the first message
+                message = messages[0]
+                # Yay! We got a new message!
+                # We retrieve the Facebook user ID of the sender
+                fb_id = message['sender']['id']
+                # We retrieve the message content
+                text = message['message']['text']
+                # Let's forward the message to the Wit.ai Bot Engine
+                # We handle the response in the function send()
+                client.run_actions(session_id=fb_id, message=text)
+    else:
+        # Returned another event
+        return 'Received Different Event'
+    return None
+
+
+def fb_message(sender_id, text):
+    """
+    Function for returning response to messenger
+    """
+    data = {
+        'recipient': {'id': sender_id},
+        'message': {'text': text}
+    }
+    # Setup the query string with your PAGE TOKEN
+    qs = 'access_token=' + FB_ACCESS_TOKEN
+    # Send POST request to messenger
+    resp = requests.post('https://graph.facebook.com/me/messages?' + qs,
+                         json=data)
+    return resp.content
 
 
 def first_entity_value(entities, entity):
-    # return the value of the entitie with the best score
+    """
+    Returns first entity value
+    """
     if entity not in entities:
         return None
-    value = entities[entity][0]['value']
-    print('first_entity_value: ' + value)
-
-    if not value:
-        # if value is empty, no entity found
+    val = entities[entity][0]['value']
+    if not val:
         return None
-    return value['value'] if isinstance(value, dict) else value
+    return val['value'] if isinstance(val, dict) else val
 
 
 def send(request, response):
-    # print response
-    print(response['text'])
+    """
+    Sender function
+    """
+    # We use the fb_id as equal to session_id
+    fb_id = request['session_id']
+    text = response['text']
+    # send message
+    fb_message(fb_id, text)
 
 
-def merge(request):
-    # get the context, type
-    context = request['context']
-    entities = request['entities']
-    print('In merge(), context: ' + str(context) +
-          ' entities:' + str(entities))
-
-    if 'place' in context:
-        del context['place']
-    category = first_entity_value(entities, 'category')
-    if category:
-        context['cat'] = category
-    if 'ack' in context:
-        print(context['ack'])
-        del context['ack']
-    return context
-
-
-def select_place(request):
-    context = request['context']
-    data = fb.getDataPage(context['cat'])
-    if data is not None:
-        i = random.randrange(0, len(data) - 1, 1)
-        context['place'] = data[i]['name']
-        return context
-    else:
-        context['place'] = 'No place found'
-        return context
-
-
-def sendErrorMessage(error):
-    print('Error')
-
-
+# Setup Actions
 actions = {
     'send': send,
-    'merge': merge,
-    'select-place': select_place,
 }
 
+# Setup Wit Client
 client = Wit(access_token=WIT_TOKEN, actions=actions)
-client.interactive()
+
+if __name__ == '__main__':
+    # Run Server
+    app.run(host='0.0.0.0', port=argv[1])
