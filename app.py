7ab993883dfdae9f02a22c617f8ca0c7f41117dd
Edisson Reinozo <edisson350@gmail.com>
2017-05-21 18:24:49 -0500

    2

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def fb_generic_message(sender_id):
                 "default_action": {
                   "type": "web_url",
                   "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                  "messenger_extensions": true,
+                  
                   "webview_height_ratio": "tall",
                   "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
                 },
